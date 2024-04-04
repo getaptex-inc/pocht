cb80eb7663cf5a3cf5a175e536daca1ac29c47e9
Student <Student@tkfk.local>
2024-04-04 09:55:13 +0500

    added rickroll

--- hw.py
+++ hw.py
@@ -1,6 +1,12 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
+from model.models import Artist, Album, Song, db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///music.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# связываем приложение и экземпляр SQLAlchemy
+db.init_app(app)
 
 @app.route('/')
 def home():
@@ -24,7 +30,7 @@ def login():
         username = request.form['username']
         password = request.form['password']
         # проверка логина и пароля
-        return render_template('about.html')
+        return redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
     else:
         return render_template('login.html')
     
@@ -32,5 +38,10 @@ def login():
 def about():
     return render_template('about.html')
 
+@app.route('/songs')
+def songs():
+    songs_list = Song.query.all()
+    return render_template('songs.html', songs=songs_list)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=8000, debug=True)
